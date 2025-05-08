13a1a2963f9640120c16a5a9eb352e62683141db
Dailylulll <fiascolull@gmail.com>
2025-05-07 20:17:30 -0500

    added error message to mongoose connection

--- server/config/db.js
+++ server/config/db.js
@@ -17,7 +17,7 @@ const connectDB = async () => {
     });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err.message);
+    console.error('Error connecting to MongoDB:', err.message);
     process.exit(1);
   }
 };
